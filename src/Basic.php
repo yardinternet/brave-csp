6569d1df619ac6193bb3c93c55ee8b7ff2861c7d
Simon <simon@yard.nl>
2024-12-02 22:30:04 +0100

    (feat): implement basic policy

---
+++ src/Basic.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Yard\Csp\Policies;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Keyword;
+use Spatie\Csp\Policies\Policy;
+use Spatie\Csp\Scheme;
+use Spatie\Csp\Value;
+
+class Basic extends Policy
+{
+	public function configure()
+	{
+		$this
+			->addDirective(Directive::BASE, Keyword::SELF)
+			->addDirective(Directive::CONNECT, Keyword::SELF)
+			->addDirective(Directive::DEFAULT, Keyword::SELF)
+			->addDirective(Directive::FONT, Keyword::SELF)
+			->addDirective(Directive::FORM_ACTION, Keyword::SELF)
+			->addDirective(Directive::IMG, Keyword::SELF)
+			->addDirective(Directive::MEDIA, Keyword::SELF)
+			->addDirective(Directive::OBJECT, Keyword::SELF)
+			->addDirective(Directive::SCRIPT, Keyword::SELF)
+			->addDirective(Directive::STYLE, Keyword::SELF)
+
+			// Prevent clickjacking by not allowing page to be embedded
+			->addDirective(Directive::FRAME_ANCESTORS, Keyword::SELF)
+
+			// GravityForms AJAX requires iframes of itself
+			->addDirective(Directive::FRAME, Keyword::SELF)
+
+			// Allow iframes for recaptcha
+			->addDirective(Directive::FRAME, [ 'https://www.google.com/recaptcha/', 'https://recaptcha.google.com/recaptcha/', ])
+
+			// Allow embedded base64 encoded images and fonts
+			->addDirective(Directive::FONT, Scheme::DATA)
+			->addDirective(Directive::IMG, Scheme::DATA)
+			->addDirective(Directive::UPGRADE_INSECURE_REQUESTS, Value::NO_VALUE)
+
+			// There's no filter for inline styles in WordPress
+			->addDirective(Directive::STYLE, Keyword::UNSAFE_INLINE)
+
+			// Video embeds
+			->addDirective(
+				Directive::FRAME,
+				[
+					'https://player.vimeo.com',
+					'https://www.youtube.com',
+					'https://www.youtube-nocookie.com',
+				]
+			)
+			->addDirective(
+				Directive::IMG,
+				[
+					'https://img.youtube.com',
+					'https://i.ytimg.com',
+					'https://i.vimeocdn.com',
+				]
+			)
+
+			// Gravatar
+			->addDirective(Directive::IMG, 'https://secure.gravatar.com')
+
+			// Google Analytics 4
+			->addDirective(
+				Directive::IMG,
+				[
+					'https://*.google-analytics.com',
+					'https://*.googletagmanager.com',
+				]
+			)
+			->addDirective(
+				Directive::CONNECT,
+				[
+					'https://*.google-analytics.com',
+					'https://*.analytics.google.com',
+					'https://*.googletagmanager.com',
+				]
+			)
+
+			// Font Awesome
+			->addDirective(
+				Directive::CONNECT,
+				[
+					'https://ka-p.fontawesome.com',
+					'https://kit.fontawesome.com',
+				]
+			)
+			->addDirective(Directive::FONT, 'https://ka-p.fontawesome.com')
+
+			// Google Fonts
+			->addDirective(Directive::FONT, 'https://fonts.gstatic.com')
+			->addDirective(Directive::STYLE, 'https://fonts.googleapis.com/css2')
+
+			// Adobe Fonts
+			->addDirective(Directive::FONT, 'https://use.typekit.net')
+			->addDirective(Directive::STYLE, 'https://*.typekit.net');
+
+		// Admin side requires unsafe-inline which doesn't work together with nonces or strict-dynamic
+		if (is_admin()) {
+			$this->addDirective(Directive::SCRIPT, Keyword::UNSAFE_INLINE);
+		} else {
+			// Propagate trust for scripts with a nonce
+			// TODO: re-activate code when FacetWP registers scripts by WP practices
+			// $this->addNonceForDirective(Directive::SCRIPT);
+			$this->addDirective(
+				Directive::SCRIPT,
+				[
+					// Keyword::STRICT_DYNAMIC,
+					Keyword::UNSAFE_INLINE,
+					'https:',
+				]
+			);
+		}
+	}
+}
