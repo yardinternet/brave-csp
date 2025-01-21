fda2e2f031cfc2dfb9444c7fbdef52b648872c08
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-01-21 10:47:34 +0100

    (chore): add and apply php cs fixer

--- src/Basic.php
+++ src/Basic.php
@@ -99,9 +99,8 @@ class Basic extends Policy
 			->addDirective(Directive::FONT, 'https://use.typekit.net')
 			->addDirective(Directive::STYLE, 'https://*.typekit.net')
 
-			
 			// ElasticSearch
-			->addDirective(Directive::CONNECT,env('WP_ENV') == 'production' ? 'https://es.elk01.yard.nl/' : 'https://es.test01.yard.nl')
+			->addDirective(Directive::CONNECT, env('WP_ENV') == 'production' ? 'https://es.elk01.yard.nl/' : 'https://es.test01.yard.nl')
 
 			// Google Translate
 			->addDirective(Directive::CONNECT, [
