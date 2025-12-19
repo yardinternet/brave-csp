4ca067c395ca7ebfde26288d156f87a9a502e86c
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-12-19 13:11:08 +0100

    test: add error

--- src/Basic.php
+++ src/Basic.php
@@ -144,6 +144,8 @@ class Basic extends Policy
 			])
 			->addDirective(Directive::FRAME, 'https://translate.googleapis.com')
 
+
+
 			// Google Maps
 			->addDirective(Directive::CONNECT, 'https://maps.googleapis.com')
 			->addDirective(Directive::IMG, [
